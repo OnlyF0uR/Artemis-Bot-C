5d63d7e52aa0eed509eff971f6063e9279bc18aa
jerskisnow <jerskisnow@protonmail.com>
2022-02-16 20:20:26 +0100

    Added global command support

--- cmds.c
+++ cmds.c
@@ -11,12 +11,20 @@ void createSlashCommands(struct discord* client, u64_snowflake_t* appId, u64_sno
     struct discord_create_guild_application_command_params params = {
       .type = DISCORD_APPLICATION_COMMAND_CHAT_INPUT,
       .name = "info",
-      .description = "Gather information about this bot.",
+      .description = "Retrieve information about this bot.",
       .default_permission = true
     };
 
     discord_create_guild_application_command(client, app_id, guild_id, &params, NULL);
   } else {
     // Register gloval commands
+    struct discord_create_global_application_command_params params = {
+      .type = DISCORD_APPLICATION_COMMAND_CHAT_INPUT,
+      .name = "info",
+      .description = "Retrieve information about this bot.",
+      .default_permission = true
+    };
+
+    discord_create_global_application_command(client, app_id, &params);
   }
 }
